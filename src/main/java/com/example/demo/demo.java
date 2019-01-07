6f12309dfbbd240bec1b37ff3cae86952695a99d
ysc <2978790137@qq.com>
2019-01-07 11:38:51 +0800

    修正war

---
+++ src/main/java/com/example/demo/demo.java
@@ -0,0 +1,27 @@
+package main.java.com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+@SpringBootApplication
+public class demo {
+	@RequestMapping("/hello")
+    @ResponseBody
+    String home() {
+        return "index.html";
+    }
+	public static void main(String[] args) {
+		SpringApplication.run(demo.class, args);
+	}
+}
+
+
+
+
